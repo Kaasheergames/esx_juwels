c933ed5b9ed1e587d79c9ddf2d5372cac98278d0
Kaasheergames <121974307+Kaasheergames@users.noreply.github.com>
2023-01-26 15:52:41 +0100

    Add files via upload

--- server/server.lua
+++ server/server.lua
@@ -4,26 +4,28 @@ TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj 
 end)
 
-local goldbar = "goldbar"
-local jewel = "jewels"
+--Config
+local goldbar = GoldbarItem
+local jewel = Config.JuwelenItem
+--
 
 RegisterServerEvent("esx_juwels:koopjuwelen")
 AddEventHandler("esx_juwels:koopjuwelen", function()
     local xPlayer = ESX.GetPlayerFromId(source)
     local inventory = xPlayer.getAccount("black_money").money
-    local amount = 50000
+    local amount = Config.GeldPerJuwelen
     local itemamount = xPlayer.getInventoryItem(jewel)
 
     if inventory >= amount then
         if itemamount.count <=450 then
             xPlayer.removeAccountMoney("black_money", amount)
             xPlayer.addInventoryItem(jewel, 50)
-            TriggerClientEvent('esx:showNotification', source, '~g~Je hebt 50 juwelen gekocht.')
+            TriggerClientEvent('esx:showNotification', source, Config.JuwelenGekocht)
         else
-            TriggerClientEvent('esx:showNotification', source, '~r~Je hebt teveel juwelen bij je.')
+            TriggerClientEvent('esx:showNotification', source, Config.TeveelJuwelen)
         end
     else
-        TriggerClientEvent('esx:showNotification', source, '~r~Je hebt niet genoeg geld bij je.')
+        TriggerClientEvent('esx:showNotification', source, Config.GeenGeld)
     end
 end)
 
@@ -31,18 +33,18 @@ RegisterServerEvent("esx_juwels:smeltjuwelen")
 AddEventHandler("esx_juwels:smeltjuwelen", function()
     local xPlayer = ESX.GetPlayerFromId(source)
     local inventory = xPlayer.getInventoryItem(jewel)
-    local itemamount = xPlayer.getInventoryItem("golbar")
+    local itemamount = xPlayer.getInventoryItem(goldbar)
     local amount = 50
 
     if itemamount.count >= 10 then
-        TriggerClientEvent('esx:showNotification', source, '~r~Je hebt teveel goudstafen bij je.')
+        TriggerClientEvent('esx:showNotification', source, Config.TeveelGoud)
     else
         if inventory.count >= 50 then
             xPlayer.removeInventoryItem(jewel, amount)
             xPlayer.addInventoryItem(goldbar, 1)
-            TriggerClientEvent('esx:showNotifcation', source, '~g~Je hebt juwelen omgesmolten naar een goudstaaf.')
+            TriggerClientEvent('esx:showNotifcation', source, Config.GoudGesmolten)
         else
-            TriggerClientEvent('esx:showNotification', source, '~r~Je hebt te weinig juwelen bij je.')
+            TriggerClientEvent('esx:showNotification', source, Config.GeenGoud)
         end
     end
 end)
